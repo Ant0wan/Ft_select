7f71a935186c9622271bad24ff30cc1162998696
Antoine Barthelemy <abarthel@student.42.fr>
2020-05-01 10:24:18 +0200

    fix(tty): adding full screen mode

--- src/term.c
+++ src/term.c
@@ -6,11 +6,12 @@
 /*   By: abarthel <abarthel@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/09/11 10:19:21 by abarthel          #+#    #+#             */
-/*   Updated: 2019/09/11 15:59:08 by abarthel         ###   ########.fr       */
+/*   Updated: 2020/05/01 10:16:38 by abarthel         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <unistd.h>
+#include <term.h>
 
 #include "libtc.h"
 
@@ -18,14 +19,14 @@ void	init_term(_Bool start)
 {
 	if (start)
 	{
+		tputs(tgetstr("ti", NULL), g_tc_fd, tc_output);
 		tc_setnoncanonical(STDIN_FILENO, 0);
 		tc_cursor(0);
-		tc_clear();
 	}
 	else
 	{
-		tc_wipe();
 		tc_cursor(1);
+		tputs(tgetstr("te", NULL), g_tc_fd, tc_output);
 		tc_setnoncanonical(STDIN_FILENO, 1);
 	}
 }
